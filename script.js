eecbe8950ed41204251aadd5ccd1e8863035b040
Jesse <JesseAbarca888@gmail.com>
2022-10-20 01:16:42 -0700

    add a task to local storage example

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 //Notes for Task Tracker to save tasks
 //save each task as a day
 
+let tasks = [
+    {
+        "taskName": "Study Local Storage",
+        "date": "10/20/22",
+        "priority": "important"
+    },
+    {
+        "taskName": "Study 2",
+        "date": "11/1/22",
+        "priority": "important"
+    }
+]
 
-let tasks =
-    [
-        {
-            "taskName": "Study Local Storage",
-            "date": "10/20/22",
-            "priority": "important"
-        },
-        {
-            "taskName": "Study 2",
-            "date": "10/1/22",
-            "priority": "important"
-        }
-    ]
 
-const Tasks = JSON.stringify(tasks);
-console.log(Tasks);
+let newTask = {
+    "taskName": "Study 3",
+    "date": "11/3/22",
+    "priority": "important"
+}
 
-//local storage can turn objects into a string
+const strTasks = JSON.stringify(tasks);
 
-localStorage.setItem('tasks', Tasks);
+//Now that you have initial tasks, add another task object to the array of tasks!
+localStorage.setItem('tasks', strTasks);
 
-const allTasks = localStorage.getItem('tasks');
-console.log(JSON.parse(allTasks));
+const addTask = (task) => {
+
+    //remember that the tasks are still in local storage.
+    //take the tasks out and manipulate it not as a string but an array of objects
+    //add my task
+    //stringify the array to be able to save to local storage
+    //save to local storage
+
+
+    let allTasks = JSON.parse(localStorage.getItem('tasks'));
+    allTasks.push(task);
+    let strAllTask = JSON.stringify(allTasks);
+    localStorage.setItem('tasks', strAllTask);
+    console.log(localStorage.getItem('tasks'));
+}
+
+
+addTask(newTask);
+
