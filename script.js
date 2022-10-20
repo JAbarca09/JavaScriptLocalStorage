83f62da5f26aa37b0fc288a5f94c6151e260ae4e
Jesse <JesseAbarca888@gmail.com>
2022-10-20 00:41:12 -0700

    Implement array of objects with Local Storage

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-console.log('Hello World');
+//Notes for Task Tracker to save tasks
+//save each task as a day
 
 
-let obj = {
-    "name" : "Jesse",
-    "height" : "5' 11"
-}
+let tasks =
+    [
+        {
+            "taskName": "Study Local Storage",
+            "date": "10/20/22",
+            "priority": "important"
+        },
+        {
+            "taskName": "Study 2",
+            "date": "10/1/22",
+            "priority": "important"
+        }
+    ]
+
+const Tasks = JSON.stringify(tasks);
+console.log(Tasks);
 
 //local storage can turn objects into a string
 
-localStorage.setItem('name', obj);
+localStorage.setItem('tasks', Tasks);
 
-const name = localStorage.getItem(obj);
-console.log(name);
+const allTasks = localStorage.getItem('tasks');
+console.log(JSON.parse(allTasks));
