b9b14aa7687c728baeaea4a8485c0646c9f4f939
Jesse <JesseAbarca888@gmail.com>
2022-10-20 00:21:21 -0700

    local storage with objects

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+
+let obj = {
+    "name" : "Jesse",
+    "height" : "5' 11"
+}
+
+//local storage can turn objects into a string
+
+localStorage.setItem('name', obj);
+
+const name = localStorage.getItem(obj);
+console.log(name);
